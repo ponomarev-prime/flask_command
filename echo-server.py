40d105aea422966885f3f199246f3decaff9b931
Alex <ax.dev@yahoo.com>
2023-07-22 01:18:10 +0300

    add some one

--- echo-server.py
+++ echo-server.py
@@ -4,14 +4,11 @@ import sys
 from markupsafe import escape
 
 app = Flask(__name__)
-#port = sys.argv[1];
-port = 5081
-
 
 @app.route("/<name>")
 def hello(name):
+    print(f"Call :: {name}")
     return f"Call :: {escape(name)}"
 
-
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=int(port))
+    app.run(debug=True, host="127.0.0.1", port=5081)
